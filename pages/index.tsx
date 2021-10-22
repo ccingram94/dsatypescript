1c3255740dfa1586972e798fdd706f6fc7158726
Constancegit status <constanceingram94@gmail.com>
2021-10-22 11:41:14 -0500

    fixed front page first block spacing

--- pages/index.tsx
+++ pages/index.tsx
@@ -25,7 +25,7 @@ const Home: NextPage = () => {
       <main className="mx-auto">
       <Header/>
         <div className="basic-box bg-cover bg-hero-img background relative bg-gradient-to-r from-red-200 via-red-300 to-yellow-200" >
-          <div className="container mx-auto flex px-5 py-48 items-center justify-center flex-col">
+          <div className="container mx-auto flex px-12 py-12 items-center justify-center flex-col">
           <h1 className="font-black text-6xl font-bold text-pink-900 text-center title-font sm:text-4x1 text-3x1 mb-4 mt-4 font-large"> 🌸 🌸 🌸 </h1>
             <h1 className="font-black text-6xl font-bold text-pink-900 text-center title-font sm:text-4x1 text-3x1 mb-4 mt-4 font-large">Data Structures and Algorithms</h1>
             <div className="flex flex-row flex-wrap justify-center items-center content-center space-x-10">
